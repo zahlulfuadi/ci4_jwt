14cb9df034a10f57633325070d2b8985390d66be
zahlulfuadi <221810676@stis.ac.id>
2020-10-18 15:26:38 +0700

    membuat method login

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -4,12 +4,89 @@ namespace App\Controllers;
 
 use App\Models\Auth_Model;
 use CodeIgniter\RESTful\ResourceController;
+use \Firebase\JWT\JWT;
 
 class Auth extends ResourceController
 {
     public function __construct()
     {
         $this->auth = new Auth_Model();
+    }
+
+    public function privateKey()
+    {
+        $privateKey = <<<EOD
+            -----BEGIN RSA PRIVATE KEY-----
+            MIICXAIBAAKBgQC8kGa1pSjbSYZVebtTRBLxBz5H4i2p/llLCrEeQhta5kaQu/Rn
+            vuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t0tyazyZ8JXw+KgXTxldMPEL9
+            5+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4ehde/zUxo6UvS7UrBQIDAQAB
+            AoGAb/MXV46XxCFRxNuB8LyAtmLDgi/xRnTAlMHjSACddwkyKem8//8eZtw9fzxz
+            bWZ/1/doQOuHBGYZU8aDzzj59FZ78dyzNFoF91hbvZKkg+6wGyd/LrGVEB+Xre0J
+            Nil0GReM2AHDNZUYRv+HYJPIOrB0CRczLQsgFJ8K6aAD6F0CQQDzbpjYdx10qgK1
+            cP59UHiHjPZYC0loEsk7s+hUmT3QHerAQJMZWC11Qrn2N+ybwwNblDKv+s5qgMQ5
+            5tNoQ9IfAkEAxkyffU6ythpg/H0Ixe1I2rd0GbF05biIzO/i77Det3n4YsJVlDck
+            ZkcvY3SK2iRIL4c9yY6hlIhs+K9wXTtGWwJBAO9Dskl48mO7woPR9uD22jDpNSwe
+            k90OMepTjzSvlhjbfuPN1IdhqvSJTDychRwn1kIJ7LQZgQ8fVz9OCFZ/6qMCQGOb
+            qaGwHmUK6xzpUbbacnYrIM6nLSkXgOAwv7XXCojvY614ILTK3iXiLBOxPu5Eu13k
+            eUz9sHyD6vkgZzjtxXECQAkp4Xerf5TGfQXGXhxIX52yH+N2LtujCdkQZjXAsGdm
+            B2zNzvrlgRmgBrklMTrMYgm1NPcW+bRLGcwgW2PTvNM=
+            -----END RSA PRIVATE KEY-----
+            EOD;
+
+            return $privateKey;
+    }
+
+    public function login()
+    {
+        $email = $this->request->getPost('email');
+        $password = $this->request->getPost('password');
+
+        // cek login 
+        $cek_login = $this->auth->cek_login($email);
+
+        if(password_verify($password, $cek_login['password'])) {
+            // sukses login 
+            // konfigurasi token
+            $secret_key = $this->privateKey();
+            $issuer_claim = "THE_CLAIM";
+            $audience_claim = "THE_AUDIENCE";
+            $issuedat_claim = time();
+            $notbefore_claim = $issuedat_claim + 10;
+            $expire_claim = $issuedat_claim + 3600;
+
+            $token = [
+                "iss" => $issuer_claim,
+                "aud" => $audience_claim,
+                "iat" => $issuedat_claim,
+                "nbf" => $notbefore_claim,
+                "exp" => $expire_claim,
+                "data" => [
+                    'id' => $cek_login['id'],
+                    'first_name' => $cek_login['first_name'],
+                    'last_name' => $cek_login['last_name'],
+                    'email' => $cek_login['email'],
+                    'password' => $cek_login['password']
+                ]
+            ];
+
+            // generate token 
+            $token = JWT::encode($token, $secret_key);
+            // jika berhasil
+            $output = [
+                'status'    => 200,
+                'message'   => 'Login sucessfully',
+                'token'     => $token,
+                'expireAt'  => $expire_claim
+            ];
+            return $this->respond($output, 200);
+        } else {
+            // gagal login
+            $output = [
+                'status'    => 401,
+                'message'   => 'Login failed'
+            ];
+            return $this->respond($output, 401);
+        }
     }
 
 	public function register()
--- app/Models/Auth_Model.php
+++ app/Models/Auth_Model.php
@@ -13,4 +13,26 @@ class Auth_Model extends Model {
         $query = $this->db->table($this->table)->insert($data);
         return $query ? true : false;
     }
+
+    public function cek_login($email)
+    {
+        $query = $this->table($this->table)
+                      ->where('email', $email)
+                      ->countAll();
+
+        // cek ada data atau tidak
+        if($query > 0) {
+            // ada data
+            $hasil = $this->table($this->table)
+                          ->where('email', $email)
+                          ->limit(1)
+                          ->get()
+                          ->getRowArray();
+        } else {
+            // tidak ada data 
+            $hasil = array();
+        }
+
+        return $hasil;
+    }
 }

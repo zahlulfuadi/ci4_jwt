3e548f9c0a9a23d23bae8a76fb38d378ab7d6b80
zahlulfuadi <221810676@stis.ac.id>
2020-10-18 15:44:05 +0700

    jwt selesai dipasang

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,10 +1,60 @@
 <?php namespace App\Controllers;
 
-class Home extends BaseController
+use \Firebase\JWT\JWT;
+use App\Controllers\Auth;
+use CodeIgniter\RESTful\ResourceController;
+
+// header
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Method: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Header: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+class Home extends ResourceController
 {
+	public function __construct()
+	{
+		$this->protect = new Auth();
+	}
+
 	public function index()
 	{
-		return view('welcome_message');
+		// ambil secret key dari controller auth 
+		$secret_key = $this->protect->privateKey();
+
+		$token = null;
+
+		$authHeader = $this->request->getServer('HTTP_AUTHORIZATION');
+
+		$arr = explode(" ", $authHeader);
+
+		$token = $arr[1];
+
+		if($token){
+			try{
+				$decode = JWT::decode($token, $secret_key, array('HS256'));
+
+				// pengecekan jika telah dideskripsi
+				if($decode){
+					// halaman akses
+					// crud mengelola database
+					$output = [
+						'message'	=> 'Access granted'
+					];
+					return $this->respond($output, 200);
+				}
+			} catch(\Exception $e){
+				// kalau token salah atau expired
+
+				$output = [
+					'message'	=> 'Access denied',
+					'error'		=> $e->getMessage()
+				];
+				return $this->respond($output, 401);
+			}
+		}
 	}
 
 	//--------------------------------------------------------------------
